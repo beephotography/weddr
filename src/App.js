17f7ce8b1eba7f79890303b04d7251b8feb9e34a
Toby-Ra <MY_NAME@example.com>
2024-07-14 17:25:51 +0200

    begin with dynamic city

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ import axios from 'axios';
 
 function App() {
   const [data, setData] = useState(null);
+  const [cityList, setCityList] = useState(
+      {
+          "latitude": "48.5459",
+          "longitude": "10.8518"
+      }
+  );
 
   useEffect(() => {
-    axios.get('https://api.open-meteo.com/v1/forecast?latitude=48.5459&longitude=10.8518&current_weather=true')
+    axios.get('https://api.open-meteo.com/v1/forecast?latitude='+cityList.latitude+'&longitude='+cityList.longitude+'&current_weather=true')
       .then(response => setData(response.data))
       .catch(error => console.error('Error fetching data:', error));
-  }, []);
+  }, [cityList]);
 
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="bg-white p-6 rounded shadow-md text-center">
+          <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+              onClick={() => setCityList({
+                  "latitude": "48.42432",
+                  "longitude": "10.87273"
+              })}>
+              Update City
+          </button>
         <h1 className="text-2xl font-bold mb-4">Wetterdaten</h1>
+
         {data ? (
           <div>
             <p>Temperatur: {data.current_weather.temperature}°C</p>
